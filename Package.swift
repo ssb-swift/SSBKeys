48cba12ddac48fe4aaa859fcc3083cd3720e1ca5
David Gomez <dg.dave@hey.com>
2020-07-11 12:27:46 -0400

    Add swift-crypto dependency

--- Package.swift
+++ Package.swift
@@ -5,24 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "SSBKeys",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "SSBKeys",
-            targets: ["SSBKeys"]),
+        .library(name: "SSBKeys", targets: ["SSBKeys"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", .exact("1.1.0-rc.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "SSBKeys",
-            dependencies: []),
-        .testTarget(
-            name: "SSBKeysTests",
-            dependencies: ["SSBKeys"]),
+        // Targets can depend on other targets in this package, and on products in packages which this package depends
+        // on.
+        .target(name: "SSBKeys", dependencies: [
+            .product(name: "Crypto", package: "swift-crypto")
+        ]),
+        .testTarget(name: "SSBKeysTests", dependencies: ["SSBKeys"])
     ]
 )
