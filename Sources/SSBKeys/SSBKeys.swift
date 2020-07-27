63ed7d79cf25d2b82f16c2d14925a06ad7ab093a
David Gomez <dg.dave@hey.com>
2020-07-27 10:53:28 -0400

    Add code documentation

--- Sources/SSBKeys/SSBKeys.swift
+++ Sources/SSBKeys/SSBKeys.swift
@@ -12,9 +12,12 @@
 import Foundation
 import Crypto
 
+/// Encryption types supported by SSBKeys.
 enum Encryption: String, Codable {
+    /// EdDSA signature scheme using SHA-512 (SHA-2) and Curve25519.
     case ed25519
 }
+
 public struct Keys {
     let encryption: Encryption
     let privateKey: Curve25519.Signing.PrivateKey
@@ -29,6 +32,15 @@ public struct Keys {
 }
 
 extension Keys {
+    /**
+     Initializes a new private key using Ed25519 over Curve25519 as encryption type with a random seed or using an optional seed.
+     
+     - Parameters:
+        - encryption: Encryption type. Default value is `.ed25519`.
+        - seed: Optional 32-byte [Data](https://developer.apple.com/documentation/foundation/data) type buffer.
+     
+     - Returns:A Keys type with EdDSA signature scheme using SHA-512 (SHA-2) over Curve25519 by default (no other type of encryption is supported yet), and accepts an optional seed that has to be a 32-byte Data type buffer.
+     */
     init(encryption: Encryption = .ed25519, seed: Data? = nil) {
         let privateKey: Curve25519.Signing.PrivateKey
 
@@ -45,6 +57,13 @@ extension Keys {
 }
 
 extension Keys: Decodable {
+    /**
+     Conforms to Codable *<[Decodable](https://developer.apple.com/documentation/swift/decodable)>* protocol creating a new Keys instance from an external representation like a JSON string.
+     
+     - Parameter decoder: The decoder to read data from.
+     
+     - Throws: Throws an error if reading from the decoder fails, or if the data read is corrupted or otherwise invalid.
+     */
     public init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         let privateKeyValue = try values.decode(String.self, forKey: .privateKey)
@@ -57,6 +76,13 @@ extension Keys: Decodable {
 }
 
 extension Keys: Encodable {
+    /**
+     Conforms to Codable *<[Encodable](https://developer.apple.com/documentation/swift/encodable)>* protocol to transform the Keys values to strings appending the encryption type and adds a new value named `id` prefixed with the `@` symbol. If the value fails to encode anything, encoder will encode an empty keyed container in its place.
+     
+     - Parameter encoder: The encoder to write data to.
+     
+     - Throws: Throws an error if any values are invalid for the given encoder’s format.
+     */
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(encryption, forKey: .encryption)
@@ -67,6 +93,11 @@ extension Keys: Encodable {
 }
 
 extension Keys {
+    /**
+     Transform Keys to a JSON representation including an additional property named `id` which is the public key prefixed by the symbol `@`.
+     
+     - Returns: Keys as a JSON string.
+     */
     public func toJSON() -> String {
         let encoder = JSONEncoder()
         encoder.outputFormatting = .prettyPrinted
@@ -76,6 +107,8 @@ extension Keys {
         return String(data: data, encoding: .utf8)!
     }
 }
+
+// MARK: - Private
 
 private func getData(from value: String) -> Data {
     let rawValue = String(value.prefix(upTo: value.firstIndex(of: ".") ?? value.endIndex))
--- Sources/SSBKeys/Utils.swift
+++ Sources/SSBKeys/Utils.swift
@@ -1,17 +1,35 @@
 import Foundation
 import Crypto
 
-public func getTag(from id: String) -> String {
-    let index = id.index(after: id.lastIndex(of: ".") ?? id.endIndex)
-    return String(id[index..<id.endIndex])
+/**
+ Tag from a given value that corresponds to the type of encryption used to create those Keys.
+ 
+ - Parameter value: Value to get the tag from i.e., an SSB private/public key or id.
+ 
+ - Returns: Tag or encryption type used to encode the value.
+ */
+public func getTag(from value: String) -> String {
+    let index = value.index(after: value.lastIndex(of: ".") ?? value.endIndex)
+    return String(value[index..<value.endIndex])
 }
 
+/**
+ Base-64 encoded string using a SHA-2 (Secure Hash Algorithm 2) *<SHA-256>* of a given data.
+ 
+ - Parameters:
+    - data: String representation of the data to encode.
+    - encoding: String encoding property. Default value is `.utf8`.
+ 
+ - Returns: Base-64 data hash.
+ */
 public func hash(data: String, encoding: String.Encoding = .utf8) -> String {
     let encodedData = data.data(using: encoding)
     let hashedData = SHA256.hash(data: encodedData.unsafelyUnwrapped)
 
     return "\(hashedData.description.toBase64(using: encoding)).sha256"
 }
+
+// MARK: - String Extensions
 
 extension String {
     func fromBase64(using encoding: String.Encoding = .utf8) -> String {
