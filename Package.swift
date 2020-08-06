28893bda178985203929220f7a2df8460a9f5fcc
David Gomez <dg.dave@hey.com>
2020-08-05 20:17:48 -0400

    Additional platforms support

--- Package.swift
+++ Package.swift
@@ -6,7 +6,10 @@ import PackageDescription
 let package = Package(
     name: "SSBKeys",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
